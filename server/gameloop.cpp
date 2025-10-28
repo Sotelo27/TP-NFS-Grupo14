cd094ea3e522bd5defa4cfa21561caef018b8605
maycol <mmeili@fi.uba.ar>
2025-10-28 19:15:27 -0300

    feat: agrego logs en el lado del server en el gameloop.cpp

--- server/gameloop.cpp
+++ server/gameloop.cpp
@@ -8,17 +8,21 @@
 #include <utility>
 #include <vector>
 
-#include "../common/constants.h"
 #include "../common/base_protocol.h"
+#include "../common/constants.h"
 
-Gameloop::Gameloop(Game& game, ClientListProtected& clients,
-                   Queue<ClientAction>& actiones_clients):
+Gameloop::Gameloop(Game& game, ClientListProtected& clients, Queue<ClientAction>& actiones_clients):
         game(game), clients(clients), actiones_clients(actiones_clients) {}
 
 void Gameloop::procesar_actiones() {
     ClientAction action;
     while (actiones_clients.try_pop(action)) {
         try {
+            std::cout << "Received action from client " << action.id << ": "
+                      << "Type=" << static_cast<int>(action.type)
+                      << ", Username=" << action.username
+                      << ", Movement=" << static_cast<int>(action.movement) << "\n";
+
             if (action.type == ClientAction::Type::Move) {
                 // Aplicar movimiento en el dominio
                 game.apply_player_move(action.id, action.movement);
@@ -38,10 +42,11 @@ void Gameloop::procesar_actiones() {
 }
 
 void Gameloop::iteracion_game() {
-    const float dt = 0.25f; // 250 ms
+    const float dt = 0.25f;  // 250 ms
     game.update(dt);
     auto positions = game.players_positions();
     clients.broadcast_player_positions(positions);
+    std::cout << "Broadcasted positions of " << positions.size() << " players.\n";
 }
 
 void Gameloop::run() {
