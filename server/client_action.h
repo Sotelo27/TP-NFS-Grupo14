0f2da49c12718c1487ce3a9519f595c296946cc6
martin <lmsotelo@fi.uba.ar>
2025-11-01 17:35:32 -0300

    feat:actualizacion de acciones de cliente para salas

--- server/client_action.h
+++ server/client_action.h
@@ -6,17 +6,24 @@
 #include "../common/base_protocol.h"
 
 struct ClientAction {
-    enum class Type { Name, Move } type;
+    enum class Type { Name, Move, Room } type;
     size_t id;
 
     std::string username;
     Movement movement;
+
+    // Acciones de sala
+    uint8_t room_cmd{0}; // ROOM_CREATE | ROOM_JOIN
+    uint8_t room_id{0};  // válido si ROOM_JOIN
 
     ClientAction(size_t id, Movement mv)
         : type(Type::Move), id(id), movement(mv) {}
 
     ClientAction(size_t id, std::string name)
         : type(Type::Name), id(id), username(std::move(name)) {}
+
+    ClientAction(size_t id, uint8_t room_cmd, uint8_t room_id)
+        : type(Type::Room), id(id), room_cmd(room_cmd), room_id(room_id) {}
 
     ClientAction() = default;
 };
