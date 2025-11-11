339a18fc9c9d61458f520bd4935e362cfea07f21
Dayrustz7u7 <mjanampa@fi.uba.ar>
2025-11-11 19:21:17 -0300

    feat: Creo clase Checkpoint para uso de logs por el momento

---
+++ server/physics/checkpoint_entity.cpp
@@ -0,0 +1,35 @@
+#include "checkpoint_entity.h"
+#include <iostream>
+#include <utility>
+
+CheckpointEntity::CheckpointEntity(size_t id, b2Body* body, int index, std::string race_id, std::string type)
+    : Entidad(id, body),
+      index(index),
+      race_id(std::move(race_id)),
+      type_(std::move(type)) {}
+
+Entidad::Type CheckpointEntity::type() const {
+    return Type::Checkpoint;
+}
+
+int CheckpointEntity::get_index() const noexcept {
+    return index;
+}
+
+const std::string& CheckpointEntity::get_race_id() const noexcept {
+    return race_id;
+}
+
+const std::string& CheckpointEntity::get_cp_type() const noexcept {
+    return type_;
+}
+
+void CheckpointEntity::onCollision(Entidad* other) {
+    if (!other) return;
+    if (other->type() == Type::Car) {
+        std::cout << "Car ID " << other->get_id()
+                  << " atravesó checkpoint " << get_index()
+                  << " (race " << get_race_id()
+                  << ", tipo=" << get_cp_type() << ")\n";
+    }
+}
