2c4826665a3a2dfd217a3bd59ce2b6b5f04e6cd3
maycol <mmeili@fi.uba.ar>
2025-11-12 13:43:26 -0300

    fix: se aplica clant format a mini_map_hud

--- client/Game/resources/mini_map_hud.cpp
+++ client/Game/resources/mini_map_hud.cpp
@@ -12,8 +12,8 @@
 
 MiniMap::MiniMap(size_t client_id, const SdlWindow& window, const MapsTextures& map_manager,
                  std::unordered_map<size_t, CarInfoGame>& info_players):
-        texture_cars_mini_map(CARS_MINI_MAP, window, Rgb(BACKGROUND_COLOR_R, BACKGROUND_COLOR_G,
-                                                         BACKGROUND_COLOR_B)),
+        texture_cars_mini_map(CARS_MINI_MAP, window,
+                              Rgb(BACKGROUND_COLOR_R, BACKGROUND_COLOR_G, BACKGROUND_COLOR_B)),
         draw_fill(window),
         client_id(client_id),
         map_manager(map_manager),
@@ -57,27 +57,29 @@ void MiniMap::renderCarOnMiniMap(int x_dest_mini_map, int y_dest_mini_map, int m
             y_dest_mini_map + (info_car.y * mini_map_height) / map_manager.getCurrentMapHeight();
 
     int size_car_point_width = 25;
-    int size_car_point_height = static_cast<float>(size_car_point_width) * HEIGHT_CAR_TEXTURE / WIDTH_CAR_TEXTURE;  // Keep aspect ratio
+    int size_car_point_height = static_cast<float>(size_car_point_width) * HEIGHT_CAR_TEXTURE /
+                                WIDTH_CAR_TEXTURE;  // Keep aspect ratio
     Area car_dest_area_mini_map(x_car_mini_map - (size_car_point_width / 2),
                                 y_car_mini_map - (size_car_point_height / 2), size_car_point_width,
                                 size_car_point_height);
 
-    texture_cars_mini_map.renderEntity(red ? AREA_RED_CAR_MINI_MAP : AREA_BLUE_CAR_MINI_MAP, car_dest_area_mini_map, info_car.angle);
+    texture_cars_mini_map.renderEntity(red ? AREA_RED_CAR_MINI_MAP : AREA_BLUE_CAR_MINI_MAP,
+                                       car_dest_area_mini_map, info_car.angle);
 }
 
 void MiniMap::renderPositionsMiniMap(int x_dest_mini_map, int y_dest_mini_map, int mini_map_width,
-                                    int mini_map_height) {
-    for (const auto& [player_id, car_info] : info_players) {
+                                     int mini_map_height) {
+    for (const auto& [player_id, car_info]: info_players) {
         if (player_id == client_id) {
             continue;
         }
 
         const PlayerTickInfo& info_car = car_info.info_car;
         renderCarOnMiniMap(x_dest_mini_map, y_dest_mini_map, mini_map_width, mini_map_height,
-                               info_car, false);
+                           info_car, false);
     }
 
     const PlayerTickInfo& info_my_car = info_players[client_id].info_car;
     renderCarOnMiniMap(x_dest_mini_map, y_dest_mini_map, mini_map_width, mini_map_height,
-                           info_my_car, true);
+                       info_my_car, true);
 }
