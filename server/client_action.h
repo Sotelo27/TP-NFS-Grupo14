5b5473000e8de1294a6c13701c7bb4842319498a
martin <lmsotelo@fi.uba.ar>
2025-11-12 02:34:47 -0300

    feat:agrego nueva accion para comenzar partida y su constructor

--- server/client_action.h
+++ server/client_action.h
@@ -5,11 +5,12 @@
 #include <cstdint>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "../common/dto/movement.h"
 
 struct ClientAction {
-    enum class Type { Name, Move, Room } type;
+    enum class Type { Name, Move, Room, StartGame } type;
     size_t id;
 
     std::string username;
@@ -18,6 +19,9 @@ struct ClientAction {
     // Acciones de sala
     uint8_t room_cmd{0};
     uint8_t room_id{0};
+    
+    // StartGame
+    std::vector<std::pair<std::string, uint8_t>> races;
 
     ClientAction(size_t id, Movement mv): type(Type::Move), id(id), movement(mv) {}
 
@@ -26,6 +30,10 @@ struct ClientAction {
 
     ClientAction(size_t id, uint8_t room_cmd, uint8_t room_id):
             type(Type::Room), id(id), movement(), room_cmd(room_cmd), room_id(room_id) {}
+    
+    //constructor para StartGame
+    ClientAction(size_t id, std::vector<std::pair<std::string, uint8_t>> races):
+            type(Type::StartGame), id(id), movement(), races(std::move(races)) {}
 
     ClientAction() = default;
 };
