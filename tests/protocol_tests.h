7cbd2fa00ba904a955ab971fba5e0db1cdc4cc2e
sotelo27 <lmsotelo@fi.uba.ar>
2025-11-30 17:44:07 -0300

    feat:declaro tests en el .h

--- tests/protocol_tests.h
+++ tests/protocol_tests.h
@@ -11,5 +11,15 @@ void test_send_and_receive_race_start();
 void test_send_and_receive_results();
 void test_send_and_receive_improvement_ok();
 void test_send_and_receive_players_list();
+void test_send_move();
+void test_send_choose_car();
+void test_send_improvement();
+void test_send_cheat();
+void test_send_exit();
+void test_send_and_receive_map_info();
+void test_send_and_receive_game_over();
+void test_send_and_receive_player_name();
+void test_send_and_receive_market_time();
+void test_send_and_receive_result_race_current();
 
 #endif //PROTOCOL_TESTS_H
