07e9f0bfe4b0bb57991aca207e12d20a463c2342
maycol <mmeili@fi.uba.ar>
2025-12-01 02:25:12 -0300

    feat: los efectos de sonidos son dinamicos

--- client/Game/resources/event_resolver.h
+++ client/Game/resources/event_resolver.h
@@ -15,16 +15,18 @@ private:
     size_t client_id;
     std::unordered_map<size_t, CarInfoGame>& info_players;
     SdlAudioManager& audio_manager;
-    std::unordered_map<EventRaceType, std::function<void()>> event_dispatch;
+    std::unordered_map<EventRaceType, std::function<void(EventInfo&)>> event_dispatch;
 
     void sounds_initialization();
     void event_dispatch_init();
 
     std::string to_string(EventRaceType e);
 
-    void event_damaged();
-    void event_checkpoint_passed();
+    void event_damaged(EventInfo& event);
+    bool setup_sound_volume_based_on_distance(const std::string& id_sound, size_t event_player_id);
+    void event_checkpoint_passed(EventInfo& event);
 
+    int distance(int x1, int y1, int x2, int y2);
 public:
     explicit EventResolver(SdlAudioManager& audio_manager,
                            std::unordered_map<size_t, CarInfoGame>& info_players, size_t client_id);
